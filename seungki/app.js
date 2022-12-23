19b2b7af157f898a039bf3f8360eef2edec7daab
seuungkei <111074566+seuungkei@users.noreply.github.com>
2022-12-23 19:25:22 +0900

    Update app.js

--- seungki/app.js
+++ seungki/app.js
@@ -17,7 +17,7 @@ const mysqlDatabase = new DataSource({
   port: process.env.TYPEORM_PORT,
   username: process.env.TYPEORM_USERNAME,
   password: process.env.TYPEORM_PASSWORD,
-  database: process.env.TYPEORM_DATABASE,
+  database: process.env.TYPEORM_DATABASE
 });
 
 mysqlDatabase
