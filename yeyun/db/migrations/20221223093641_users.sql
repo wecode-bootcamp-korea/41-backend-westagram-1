bb391c1d80f5f579aa520a0c5a3d95cc3e2c3a1f
예윤 <yunye@yeyun-ui-MacBookAir.local>
2022-12-24 14:07:06 +0900

    전체 게시물 조회

--- yeyun/db/migrations/20221223093641_users.sql
+++ yeyun/db/migrations/20221223093641_users.sql
@@ -4,6 +4,7 @@ CREATE TABLE users(
   name VARCHAR(50) NOT NULL,
   email VARCHAR(50) NOT NULL,
   password VARCHAR(50) NOT NULL
+  
 );
 
 -- migrate:down
--- yeyun/db/migrations/20221223110736_posts.sql
+++ yeyun/db/migrations/20221223110736_posts.sql
@@ -3,7 +3,9 @@ CREATE TABLE posts(
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(100) NOT NULL,
   content VARCHAR(2000) NULL,
-  user_id INT NOT NULL
+  user_id INT NOT NULL,
+  CONSTRAINT posts_fk FOREIGN KEY (user_id) REFERENCES users(id)
+  
 );
 
 -- migrate:down
