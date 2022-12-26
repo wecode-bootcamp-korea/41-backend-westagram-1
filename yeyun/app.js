088d7f56bb26ce806ed9d74c56600245220445e4
예윤 <yunye@yeyun-ui-MacBookAir.local>
2022-12-26 22:27:49 +0900

    cache clear

--- yeyun/app.js
+++ yeyun/app.js
@@ -52,17 +52,16 @@ app.post("/users", async (req, res, next) => {
 });
 
 app.post("/posts", async (req, res, next) => {
-  const { title, content, user_id, url } = req.body;
+  const { title, content, user_id } = req.body;
 
   await appDataSource.query(
     `INSERT INTO posts (
       title,
       content,
-      user_id,
-      url
-    ) VALUES (?, ?, ?, ?);
+      user_id
+    ) VALUES (?, ?, ?);
     `,
-    [title, content, user_id, url]
+    [title, content, user_id]
   );
 
   res.status(201).json({ message: "postCreated" });
--- yeyun/db/migrations/20221226130915_create_posts_table.sql
+++ yeyun/db/migrations/20221226130915_create_posts_table.sql
@@ -2,10 +2,10 @@
 CREATE TABLE posts(
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(100) NOT NULL,
-  content VARCHAR(2000) NULL,
+  content VARCHAR(3000) NULL,
   user_id INT NOT NULL,
-  url VARCHAR(2000) NOT NULL,
-  CONSTRAINT posts_fk FOREIGN KEY (user_id) REFERENCES users(id)
+  created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+  FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
 -- migrate:down
