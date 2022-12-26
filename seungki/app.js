cf4ca71001b81c31c209c49ef596cf8c99a8e9fa
seuungkei <seuungkei@gmail.com>
2022-12-26 19:07:11 +0900

    Bcrypt를 이용한 비밀번호 암호화

--- seungki/app.js
+++ seungki/app.js
@@ -1,15 +1,16 @@
-const http = require("http");
-
 require('dotenv').config();
-const express = require("express");
-const cors = require("cors"); 
-const morgan = require("morgan");
-const { DataSource } = require("typeorm");
+
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const bcrypt = require('bcrypt');
+const { DataSource } = require('typeorm');
 
 const app = express();
+
 app.use(express.json());
 app.use(cors());
-app.use(morgan("dev"));
+app.use(morgan('dev'));
 
 const mysqlDatabase = new DataSource({
   type: process.env.TYPEORM_CONNECTION,
@@ -17,27 +18,30 @@ const mysqlDatabase = new DataSource({
   port: process.env.TYPEORM_PORT,
   username: process.env.TYPEORM_USERNAME,
   password: process.env.TYPEORM_PASSWORD,
-  database: process.env.TYPEORM_DATABASE
+  database: process.env.TYPEORM_DATABASE,
 });
 
-mysqlDatabase.initialize()
-.then(() => {
-  console.log("Data Source has been inilialized!💡")
-})
-.catch((err) => {
-  console.error("Error during Data Source initialization", err)
-  mysqlDatabase.destroy()
-})
+mysqlDatabase
+  .initialize()
+  .then(() => {
+    console.log('Data Source has been inilialized!💡');
+  })
+  .catch((err) => {
+    console.error('Error during Data Source initialization', err);
+    mysqlDatabase.destroy();
+  });
 
 // Health Check
-app.get("/ping", (req, res) => {
-res.status(200).json({"message" : "pongsssss"});
+app.get('/ping', (req, res) => {
+  res.status(200).json({ message: 'pongsssss' });
 });
 
-
-// 유저 회원가입
-app.post("/email_signup", async (req, res) => {
+// 유저 회원가입 & Bcrypt를 이용하여 비밀번호 암호화하기
+app.post('/email_signup', async (req, res) => {
   const { name, email, profileImage, password } = req.body;
+
+  const saltRounds = 12;
+  const hashedPassword = await bcrypt.hash(password, saltRounds);
 
   await mysqlDatabase.query(
     `INSERT INTO users(
@@ -47,18 +51,18 @@ app.post("/email_signup", async (req, res) => {
       password
     ) VALUES (?, ?, ?, ?);
     `,
-    [ name, email, profileImage, password ]
+    [name, email, profileImage, hashedPassword]
   );
-  res.status(201).json({message : "userCreated"})
-})
+  res.status(201).json({ message: 'userCreated' });
+});
 
 const PORT = process.env.PORT;
 
 const start = async () => {
   try {
-    app.listen(PORT, () => console.log(`server is listening on ${PORT}🔥🔥🔥🔥🔥🔥🔥`))
+    app.listen(PORT, () => console.log(`server is listening on ${PORT}🔥🔥🔥🔥🔥🔥🔥`));
   } catch (err) {
-    console.error(err)
+    console.error(err);
   }
 };
 
