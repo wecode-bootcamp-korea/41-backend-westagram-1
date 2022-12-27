a32a29f2e09d9833e33a327e78b014f70da51dc3
KIM 5EUNG <gangnam0522@gmail.com>
2022-12-27 16:04:26 +0900

    Modify: bcrypt 제거 및 불필요 코드 삭제

--- 5EUNG/app.js
+++ 5EUNG/app.js
@@ -30,61 +30,25 @@ app.use(express.json());
 app.use(cors());
 app.use(morgan('dev'))
 
-// health check 
 app.get("/ping", (req, res) => {
   res.status(200).json({ message: "pong" })
 });
 
-//////////
-//회원가입//
-//////////
 app.post('/signup', async (req, res, next) => {
-  const { id, name, email, password } = req.body
+  const { name, email, password } = req.body
 
   await appDataSource.query(
     `INSERT INTO users(
-      id,
       name,
       email,
       password
-      ) VALUES (?, ?, ?, ?);
+      ) VALUES (?, ?, ?);
     `,
-    [id, name, email, password]
+    [name, email, password]
   );
 
   res.status(201).json({ message: "successfully signed up" });
 });
-///// 회원가입 -END- /////
-
-//////////////
-//Bcrypt 실습//
-//////////////
-const bcrypt = require("bcrypt"); // (1) bcrypt 모듈 import
-
-const password = '1q2w3e4r'; // (2) 암호화 할 평문
-const saltRounds = 12; // (3) Cost Factor
-
-const makeHash = async (password, saltRounds) => {
-  return await bcrypt.hash(password, saltRounds); // (4) hash() method로 암호화, 첫번째 인자로 암호화 하고 싶은 평문이 두번째 인자로 Cost Factor가 들어간다.
-}
-///// Bcrypt 실습 -END- /////
-
-//////////////
-//Bcrypt 검증//
-//////////////
-const checkHash = async (password, hashedPassword) => {
-  return await bcrypt.compare(password, hashedPassword) // (1)
-}
-
-const main = async () => {
-  const hashedPassword = await makeHash("password", 12);
-  console.log(hashedPassword);
-  const result = await checkHash("password", hashedPassword);
-  console.log(result);
-};
-
-main();
-// Bcrypt 검증 -END- /////
 
 const PORT = process.env.PORT;
 
