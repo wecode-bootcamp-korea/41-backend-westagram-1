f314fd90e641b05bd0fc9d37d515fd6a33ee825c
예윤 <yunye@yeyun-ui-MacBookAir.local>
2022-12-28 21:28:41 +0900

    Bcrypt를 이용하여 비밀번호 암호화 하기

--- yeyun/app.js
+++ yeyun/app.js
@@ -1,11 +1,10 @@
 require("dotenv").config();
 
-require("dotenv").config();
-
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
 const { DataSource } = require("typeorm");
+const bcrypt = require("bcrypt");
 
 const app = express();
 
@@ -37,34 +36,80 @@ app.get("/ping", (req, res) => {
   res.status(200).json({ message: "pong" });
 });
 
-app.post("/users", async (req, res, next) => {
-  const { name, email, profile_image, password } = req.body;
+app.get("/posts", async (req, res) => {
+  const postData = await appDataSource.query(
+    `SELECT
+          users.id as userId,
+          users.profile_iamge as userProfileImage,
+          posts.id as postingId,
+          posts.content as postingContent
+      FROM posts
+      INNER JOIN users ON users.id = posts.user_id;
+      `,
+    (err, rows) => {
+      return res.status(200).json({ data: rows });
+    }
+  );
+});
+
+app.get("/posts/user/:userId", async (req, res) => {
+  const { userId } = req.params;
+  const pcheck = await appDataSource.query(
+    `SELECT
+        users.id as userId,
+        users.profile_iamge as userProfileImage,
+        JSON_ARRAYAGG(
+          JSON_OBJECT(
+          "postingId" , posts.id,
+          "postingImage", posts.imageurl,
+          "postContent", posts.content)
+          ) as postings
+      FROM posts
+      INNER JOIN users ON users.id = posts.user_id
+      WHERE posts.user_id = ?
+      GROUP BY posts.user_id;
+    `,
+    [userId]
+  );
+  return res.status(200).json({ data: pcheck });
+});
+
+app.post("/users", async (req, res) => {
+  const { name, email, profile_iamge, password } = req.body;
+  const saltRounds = 12;
+
+  const makeHash = async (password, saltRounds) => {
+    return await bcrypt.hash(password, saltRounds);
+  };
+
+  const hashedPassword = await makeHash(password, saltRounds);
 
   await appDataSource.query(
     `INSERT INTO users (
       name,
       email,
-      profile_image,
+      profile_iamge,
       password
     ) VALUES (?, ?, ?, ?);
     `,
-    [name, email, profile_image, password]
+    [name, email, profile_iamge, hashedPassword]
   );
 
   res.status(201).json({ message: "userCreated" });
 });
 
-app.post("/posts", async (req, res, next) => {
-  const { title, content, user_id } = req.body;
+app.post("/posts", async (req, res) => {
+  const { title, content, user_id, image_url } = req.body;
 
   await appDataSource.query(
     `INSERT INTO posts (
       title,
       content,
-      user_id
-    ) VALUES (?, ?, ?);
+      user_id,
+      image_url
+    ) VALUES (?, ?, ?, ?);
     `,
-    [title, content, user_id]
+    [title, content, user_id, image_url]
   );
 
   res.status(201).json({ message: "postCreated" });
--- yeyun/db/migrations/20221223062359_create_posts_table.sql
+++ yeyun/db/migrations/20221223062359_create_posts_table.sql
@@ -4,6 +4,7 @@ CREATE TABLE posts(
   title VARCHAR(100) NOT NULL,
   content VARCHAR(3000) NULL,
   user_id INT NOT NULL,
+  image_url VARCHAR(2000) NOT NULL,
   created_at TIMESTAMP NOT NULL DEFAULT NOW(),
   PRIMARY KEY (id),
   FOREIGN KEY (user_id) REFERENCES users(id)
